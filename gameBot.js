299eb4501dc39776acf019d183414cacede6ff57
Travis Craft <tlcraft@users.noreply.github.com>
2019-03-09 16:20:19 -0700

    Clean up bot code.

--- gameBot.js
+++ gameBot.js
@@ -1,38 +1,40 @@
-// Import the Discord API
-Discord = require("discord.js");
-https = require("https");
+discord = require('discord.js');
+https = require('https');
 
-//Connect to the API as a user.
-var bot = new Discord.Client();
+// Connect to Discord as a user
+var bot = new discord.Client();
 
-//Grab our Bot Token from the environment variable on our computer.
-const token = process.env.TEST_BOT_TOKEN;
+// Grab our Bot Token from the environment variable
+const token = process.env.DISCORD_BOT_TOKEN;
 
-//Stuff that gets run once the bot connects successfully
-bot.on('ready', () =>
-{
-  console.log("Logged in as "+bot.user.username+" "+bot.user.id+".");
+// Run once the bot connects successfully
+bot.on('ready', () => {
+  console.log('Logged in as ' + bot.user.username + '. ID: ' + bot.user.id + '.');
   console.log('I am ready!')
+  
+  // Lists the servers that this bot is active in
   exports.activeServers = bot.guilds.array();
-  for (let value of exports.activeServers) //Lists the servers that this bot is active in.
-  {
+  for (let value of exports.activeServers) {
     console.log(value.name);
   }
-})
+});
 
-//Stuff that is run each time a message is seen by the bot.
-bot.on("message", (message) =>
-{
-  if(message.content.substring(0,1) == "!") //Checks if the first letter of a message is a !
-  {
-    let commandArgs = message.content.substring(1).split(/\s+/) //Removes the ! and then breaks the rest of the message apart into chunks based on whitespace
+// Run each time a message is seen by the bot
+bot.on('message', (message) => {
+  if(isMessageForBot(message)) {
+    //Removes the bot key and then breaks the rest of the message apart into chunks based on whitespace
+    const commandArgs = message.content.substring(1).split(/\s+/);
 
-    if(commandArgs[0] == "hello") //Checks if the first word is hello
-    {
-      message.channel.send("Hello "+message.author.username+".  How are you today?");  //Sends a message in the same channel that !hello was typed.
+    if(commandArgs[0] === 'hello') {
+      //Sends a message in the same channel that !hello was typed.
+      message.channel.send('Hello ' + message.author.username + '.  How are you today?');
     }
   }
-})
+});
+
+function isMessageForBot(message) {
+  return message.content.substring(0, 1) === '!';
+}
 
 // Log our bot in
 bot.login(token);
@@ -40,7 +42,7 @@ bot.login(token);
 // Keep app awake
 setInterval(
   () => {
-    https.get("https://travislcraft-discord-game-bot.herokuapp.com/");
+    https.get('https://travislcraft-discord-game-bot.herokuapp.com/');
   },
   300000
-)
+);
